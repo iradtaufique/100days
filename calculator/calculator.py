643283a686304bfc1d55482a9037ab2bb17b7185
iradtaufique <iradukunda.ta@gmail.com>
2025-10-26 10:43:09 -0700

    added logo and use while loop to loop based on the user choice

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,5 @@
+import art
+
 def add(n1, n2):
     return n1 + n2
 
@@ -20,20 +22,33 @@ operations = {
     "/": division,
 }
 
+def calculator():
+    print(art.logo)
+    # create a variable that will change according to the user input
+    restart = True
+    # getting the first number
+    num1 = float(input('Enter the first number: '))
+    while restart:
+        # displaying available operators
+        for operator in operations:
+            print(operator)
+        symbol = input('Choose one operator from the list above: ')
+        num2 = float(input('Enter the next number: '))
 
-# getting the first number
-num1 = float(input('Enter the first number: '))
-# displaying available operators
-for operator in operations:
-    print(operator)
-symbol = input('Choose one operator from the list above: ')
-num2 = float(input('Enter the next number: '))
+        result = operations[symbol](num1, num2)
+        print(f"{num1} {symbol} {num2} = {result}")
+        user_choice = input(f"type 'y' to continue calculating with {result}, or 'n' to start new calculation: ").lower()
 
-result = operations[symbol](num1, num2)
-print(f"{num1} {symbol} {num2} = {result}")
-user_choice = input(f"type 'y' to continue calculating with {result} or 'n' to start new calculation: ").lower()
+        # check User Choice
+        if user_choice == 'y':
+            num1 = result
+        else:
+            restart = False
+            # clear the screen
+            print('\n' * 20)
+            calculator()
 
-
+calculator()
 
 
 

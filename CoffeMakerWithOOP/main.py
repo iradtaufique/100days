ab6025083a0a5efe9ef4e840263fce56c5ed7155
iradtaufique <iradukunda.ta@gmail.com>
2025-12-22 18:39:14 -0800

    dispalying report, and off functionality feature

--- CoffeMakerWithOOP/main.py
+++ CoffeMakerWithOOP/main.py
@@ -1,3 +1,19 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+
+is_on = True
+while is_on:
+    user_choice = input(f"What would you like? (espresso/latte/cappuccino/):").lower()
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        coffee_maker.report()
+    else:
+
+        if coffee_maker.is_resource_sufficient(user_choice):
+            print("Pass")
+
+
